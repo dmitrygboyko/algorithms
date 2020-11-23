d89b595076016d43fc19ec9c071c9eef8fb8194a
Dmitry Boiko <dmitrygboyko@gmail.com>
2020-11-23 18:57:03 +1100

    Initial check-in of module Algorithms

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Algorithms.Sorting;
+
+namespace algos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = new int[]{ 6, 5, 4, 3, 2, 1 };
+
+            SelectionSort.Sort(array);
+            Print(array);
+
+            Console.ReadLine();
+        }
+
+        
+
+        static void Print(int[] array)
+        {
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ Sorting/SelectionSort.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Algorithms.Sorting
+{
+    public static class SelectionSort
+    {
+        public static void Sort(int[] array)
+        {
+            for (var i = 0; i < array.Length - 1; i++)
+            {
+                var min = i;
+
+                for (var j = i + 1; j < array.Length; j++)
+                {
+                    if (array[j] < array[min])
+                    {
+                        min = j;
+                    }
+                }
+
+                if (min != i)
+                {
+                    var temp = array[i];
+                    array[i] = array[min];
+                    array[min] = temp;
+                }
+            }
+        }
+    }
+}
