14ff9b7cfae3ed684c71f550a9735d53fcff8a7e
Dmitry Boiko <dmitrygboyko@gmail.com>
2020-12-07 21:06:42 +1100

    We don't need to assign j until its final place is found

--- Sorting/InsertionSort.cs
+++ Sorting/InsertionSort.cs
@@ -1,5 +1,4 @@
-﻿using System;
-namespace Algorithms.Sorting
+﻿namespace Algorithms.Sorting
 {
     public class Insertion
     {
@@ -8,14 +7,15 @@ namespace Algorithms.Sorting
             for(var i = 1; i < array.Length; i++)
             {
                 var j = i;
+                var currentValue = array[j];
 
-                while(j > 0 && array[j] < array[j - 1])
+                while(j > 0 && currentValue < array[j - 1])
                 {
-                    var temp = array[j];
                     array[j] = array[j - 1];
-                    array[j - 1] = temp;
                     j--;
                 }
+
+                array[j] = currentValue;
             }
         }
     }
