1653e29dca748733546d2e30e099649064ac9269
Dmitry Boiko <dmitrygboyko@gmail.com>
2020-12-27 19:19:31 +1100

    Add Queue data structure

---
+++ Algorithms/DataStructures/Queue.cs
@@ -0,0 +1,87 @@
+﻿using System;
+namespace Algorithms.DataStructures
+{
+    public class Queue
+    {
+        const int defaultQueueSize = 12;
+        const int defaultGrowthFactor = 2;
+
+        object[] array;
+        int size;
+        int tail;
+        int head;
+        int growthFactor;
+
+        public Queue() : this(defaultQueueSize, defaultGrowthFactor)
+        {
+        }
+
+        public Queue(int queueSize, int growthFactor)
+        {
+            array = new object[queueSize];
+            this.growthFactor = growthFactor;
+        }
+
+        public void Enqueue(object item)
+        {
+            if (size == array.Length)
+            {
+                var newSize = array.Length * growthFactor;
+                ResizeArray(newSize);
+            }
+                
+            array[tail] = item;
+            tail = (tail + 1) % array.Length;
+            size++;
+        }
+
+        public object Dequeue ()
+        {
+            if (size == 0)
+            {
+                throw new InvalidOperationException("Queue is empty");
+            }
+
+            var dequeuedItem = array[head];
+            array[head] = null;
+            head = (head + 1) % array.Length;
+            size--;
+
+            return dequeuedItem;
+        }
+
+        public object Peek()
+        {
+            if (size == 0)
+            {
+                throw new InvalidOperationException("Queue is empty");
+            }
+
+            return array[head];
+        }
+
+        public bool IsEmpty => size == 0;
+
+        void ResizeArray(int capacity)
+        {
+            var newArray = new object[capacity];
+
+            if (size > 0)
+            {
+                if (head < tail)
+                {
+                    Array.Copy(array, head, newArray, 0, size);
+                }
+                else
+                {
+                    Array.Copy(array, head, newArray, 0, array.Length - head);
+                    Array.Copy(array, 0, newArray, array.Length - head, tail);
+                }
+
+                array = newArray;
+                head = 0;
+                tail = size;
+            }
+        }
+    }
+}
---
+++ Tests/QueueTests.cs
@@ -0,0 +1,50 @@
+﻿using Xunit;
+using Algorithms.DataStructures;
+
+namespace Tests
+{
+    public class QueueTests
+    {
+        [Fact]
+        public void Enqueue_AddsNewItemToQueue()
+        {
+            var queue = new Queue();
+
+            // Pre-condition
+            Assert.True(queue.IsEmpty);
+
+            queue.Enqueue(1);
+            Assert.False(queue.IsEmpty);
+        }
+
+        [Fact]
+        public void Dequeue_RemovesItemFromQueue()
+        {
+            var queue = new Queue();
+            queue.Enqueue(1);
+
+            // Pre-condition
+            Assert.False(queue.IsEmpty);
+
+            var dequeuedItem = queue.Dequeue();
+            Assert.Equal(1, dequeuedItem);
+            Assert.True(queue.IsEmpty);
+        }
+
+        [Fact]
+        public void QueueQworksCorrectly_When_Resized()
+        {
+            var queue = new Queue(1, 4);
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+
+            var dequeuedItem = queue.Dequeue();
+            Assert.Equal(1, dequeuedItem);
+
+            dequeuedItem = queue.Dequeue();
+            Assert.Equal(2, dequeuedItem);
+
+            Assert.True(queue.IsEmpty);
+        }
+    }
+}
