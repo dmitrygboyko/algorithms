05069ebebc5f7d8615710f0c7b55c8a8c2f0ef57
Dmitry Boiko <dmitrygboyko@gmail.com>
2021-01-06 18:53:57 +1100

    Add Equalize the Array challange solution

---
+++ Algorithms/Challenges/ProblemSolving/EqualizeTheArrayChallange.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Algorithms.Challenges.ProblemSolving
+{
+    public class EqualizeTheArrayChallange
+    {
+        public static int EqualizeArray(int[] arr)
+        {
+            const int counterArrayLength = 101;
+            var counterArray = new int[counterArrayLength];
+
+            for (var i = 0; i < arr.Length; i++)
+            {
+                counterArray[arr[i]]++;
+            }
+
+            var maxOccurance = 0;
+
+            for (var i = 0; i < counterArrayLength; i++)
+            {
+                if (counterArray[i] > maxOccurance)
+                {
+                    maxOccurance = counterArray[i];
+                }
+            }
+
+            return arr.Length - maxOccurance;
+        }
+    }
+}
